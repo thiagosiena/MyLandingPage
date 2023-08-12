2ec239934144e300bde7ea767f91186b7a919b08
thiagosiena <bedeu.siena@gmail.com>
2023-08-12 18:54:41 -0300

    header init

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,18 @@
-import '../styles/Header.scss'
-import { Link } from "react-router-dom";
-import Logo from '../imgs/parrot_PNG96591.png'
+import '../styles/Header.scss';
+import { Link } from 'react-router-dom';
+import Logo from '../imgs/LogoTSS.png';
 
 function Header() {
 
     return (<>
 
 
-        <div className="Header">
-            <div className="Contempt" >
-                <img className="Logo" src={Logo} alt='logo' />
-                <div>Projects</div>
-                <div><Link to="/Contact"> Contact</Link> </div>
+        <div className='Header'>
+            <div className='Contempt' >
+                <img className='Logo' src={Logo} alt='logo' />
+                <div className='Projects'>Projects</div>
+                <div className='About'>About</div>
+                <Link to='/Contact' className='Contact'>Contact</Link>
             </div>
         </div>
 
