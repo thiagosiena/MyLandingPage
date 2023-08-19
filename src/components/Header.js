a76763da6bc2ef92b04e85dbd9e10d1d8bf3ac45
thiagosiena <bedeu.siena@gmail.com>
2023-08-18 22:33:51 -0300

    Header Update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,33 @@
-import '../styles/Header.scss';
+import '../styles/css/Header.css';
 import { Link } from 'react-router-dom';
-import Logo from '../imgs/LogoTSS.png';
+import {DropDownItem, DropDown} from './DropDown';
+import Logo from '../imgs/App Logo Inspiraton 39.svg';
+import Calculator from '../imgs/Icons/icons8-calculator-26.png';
+import Arrow from '../imgs/Icons/icons8-arrow-50.png';
+import { useState } from "react";
 
-function Header() {
 
+export default function Header() {
+
+
+    const [OpenDropDown, SetOpenDropDown] = useState(false);
+    const ClassProjetcs = "Projects " + (OpenDropDown ? "aberto" : "");
     return (<>
-
-
+        {console.log(ClassProjetcs)}
         <div className='Header'>
-            <div className='Contempt' >
+            <div className='Content' >
                 <img className='Logo' src={Logo} alt='logo' />
-                <div className='Projects'>Projects</div>
+                <Link to="# " className={ClassProjetcs} onClick={() => SetOpenDropDown(!OpenDropDown)}>
+                    Projects
+                    <img className='Arrow' src={Arrow} alt='arrow' />
+                </Link>
+                {OpenDropDown ? (
+                    <DropDown>
+                         <DropDownItem img={Calculator} alt={"icon"} url={"/Calculator"} text={"Calculator"} />
+                        <DropDownItem text={"test"} />
+                    </DropDown>
+                    )
+                    : null}
                 <div className='About'>About</div>
                 <Link to='/Contact' className='Contact'>Contact</Link>
             </div>
@@ -21,4 +38,3 @@ function Header() {
 
 }
 
-export default Header
