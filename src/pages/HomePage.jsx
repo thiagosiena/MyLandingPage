9264b9d7fd9f152a9063ea4245af0b9350cbf77c
thiagosiena <bedeu.siena@gmail.com>
2024-04-07 16:24:01 -0300

    a

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -11,7 +11,7 @@ import React from "react";
 const HomePage = () => {
 
     const [content] = useTypewriter({
-        words: ["Thiago Siena", "", "a Developer", ""],
+        words: ["Thiago ", "", "a Developer", ""],
         loop: {},
     });
 
